f991c535876432e417356f65101f9f3e166e9712
Fabio D7 Fatec <fabio.dias7@fatec.sp.gov.br>
2021-12-05 13:30:05 -0300

    Refactoring Code

--- src/script.js
+++ src/script.js
@@ -5,8 +5,13 @@ let ctrl_time = {
 };
 
 let user_answers = [];
-let obj_question;
+let obj_questions = {
+    questions: [],
+    answers: [],
+    selectedQuestions:[]
+};
 
+const data_url = "http://localhost:8080/exam/data/";
 
 
 function showTime() {
@@ -18,15 +23,10 @@ function showTime() {
     document.querySelector("#timer > span").innerHTML = str_hour + ":" + str_minutes;
 }
 
-function startOrStopExam(bol_start) {
-    if (bol_start) {
-        ctrl_timer = setInterval(manageTime, 60000);
-    } else {
-        clearInterval(ctrl_timer);
-        ctrl_time.hour = 2;
-        ctrl_time.minutes = 0;
-        document.querySelector("#timer > span").innerHTML = "02:00";
-    }
+function resetTimer() {
+    ctrl_time.hour = 2;
+    ctrl_time.minutes = 0;
+    document.querySelector("#timer > span").innerHTML = "02:00";
 }
 
 function manageTime() {
@@ -45,30 +45,55 @@ function manageTime() {
     }
 }
 
-function handleBntTimer(obj_button) {
-    let var_startExam = obj_button.dataset.startExam;
-    if (var_startExam == "0") { //Start Exam
-        obj_button.dataset.startExam = "1";
-        startOrStopExam(true);
-        obj_button.classList.add("btn-stop-test");
-        loadAllQuestions();
-        showQuestion();
-        user_answers = [];
-        showMessageToUser("");
-        document.querySelector("#form1").classList.add("formActive");
-        obj_button.innerHTML = "Stop Exam";
-    } else { //Stop Exam
-        obj_button.dataset.startExam = "0";
-        startOrStopExam(false);
-        obj_button.classList.remove("btn-stop-test");
-        showQuestion(false);
-        document.querySelector("#form1").classList.remove("formActive");
-        obj_button.innerHTML = "Start Exam";
-    }
+
+function startExam() {
+    ctrl_timer = setInterval(manageTime, 60000);
+    obj_questions.selectedQuestions = [];
+    user_answers = [];
 }
 
-function showQuestion(bol_show = true) {
+function stopExam() {
+    clearInterval(ctrl_timer);
+    resetTimer() 
+}
+
+function checkUserResults()  {
+    let int_hits = 0;
+    let int_j;
+
+    for (let i = 0; i < user_answers.length; i++) {
+        int_j = obj_questions.selectedQuestions[i];
+        if (user_answers[i] == obj_questions.answers[int_j])
+            int_hits++;
+    }
+
+    showMessageToUser("You answered all of questions!<br />Your hints: " + int_hits.toString() );
+}
+
+
+function chooseQuestion() {
     let choosenQuestion;
+
+    while (true) {
+        choosenQuestion = Math.round(Math.random() * obj_questions.questions.length);
+
+        if (
+            (choosenQuestion >= 0 && choosenQuestion < obj_questions.questions.length) &&
+            (
+                (obj_questions.selectedQuestions.length == 0) ||
+                (!obj_questions.selectedQuestions.includes(choosenQuestion) )
+            )
+        ) {
+            break;
+        }
+    }
+    
+    obj_questions.selectedQuestions.push(choosenQuestion);
+
+    return choosenQuestion;
+}
+
+function showQuestion(choosenQuestion) {
     let str_html_options = `
         <label for="form_alt1">
             <input type="radio" value="A" id="form_alt1" name="answer" required />Alternativa A
@@ -88,67 +113,75 @@ function showQuestion(bol_show = true) {
         <input type="hidden" value="@0@" name="question_index" />
     `;
 
-    if (!bol_show) {
-        document.querySelector("#form_description").innerHTML = "Enunciado.";
-
-        document.querySelector("#form_alternatives").innerHTML = str_html_options;
-        return;
-    }
-
-    while (true) {
-        choosenQuestion = Math.round(Math.random() * obj_question.questions.length);
-
-        if (
-            (choosenQuestion >= 0 && choosenQuestion < obj_question.questions.length) &&
-            (
-                (obj_question.selectedQuestions.length == 0) ||
-                (!obj_question.selectedQuestions.find(element => element == choosenQuestion) )
-            )
-        ) {
-            break;
-        }
-    }
-    
-    obj_question.selectedQuestions.push(choosenQuestion);
-
-    let str_html_desc = obj_question.questions[choosenQuestion].description;
+    let str_html_desc = obj_questions.questions[choosenQuestion].description;
     str_html_desc = str_html_desc.replace("\n", "<br />\n");
 
     document.querySelector("#form_description").innerHTML = str_html_desc;
 
     str_html_options = str_html_options.replace(
         "Alternativa A",
-        obj_question.questions[choosenQuestion].options[0]
+        obj_questions.questions[choosenQuestion].options[0]
     );
     str_html_options = str_html_options.replace(
         "Alternativa B",
-        obj_question.questions[choosenQuestion].options[1]
+        obj_questions.questions[choosenQuestion].options[1]
     );
     str_html_options = str_html_options.replace(
         "Alternativa C",
-        obj_question.questions[choosenQuestion].options[2]
+        obj_questions.questions[choosenQuestion].options[2]
     );
     str_html_options = str_html_options.replace(
         "Alternativa D",
-        obj_question.questions[choosenQuestion].options[3]
+        obj_questions.questions[choosenQuestion].options[3]
     );
     str_html_options = str_html_options.replace(
         "Alternativa E",
-        obj_question.questions[choosenQuestion].options[4]
+        obj_questions.questions[choosenQuestion].options[4]
     );
 
     document.querySelector("#form_alternatives").innerHTML = str_html_options;
 
 }
 
+function showMessageToUser(str_message) {
+    document.querySelector("#user_message").innerHTML = str_message;
+}
+
+function handleBntTimer(obj_button) {
+
+    let var_startExam = obj_button.dataset.startExam;
+
+    if (var_startExam == "0") { //Start Exam
+        obj_button.dataset.startExam = "1";
+        startExam();
+        showMessageToUser("");
+        
+        let indexChoosenQuestion = chooseQuestion();
+        showQuestion(indexChoosenQuestion);
+        
+        document.querySelector("#form1").classList.add("formActive");
+        obj_button.innerHTML = "Stop Exam";
+        obj_button.classList.add("btn-stop-test");
+   
+    } else { //Stop Exam
+        obj_button.dataset.startExam = "0";
+        stopExam();
+        
+        document.querySelector("#form1").classList.remove("formActive");
+        obj_button.innerHTML = "Start Exam";
+        obj_button.classList.remove("btn-stop-test");
+    }
+
+}
+
 function submitAnswer(event) {
     event.preventDefault();
 
-    let obj_form = document.querySelector("form");
+    const obj_form = document.querySelector("form");
 
     user_answers.push(obj_form.answer.value);
 
-    if (user_answers.length == obj_question.answers.length) {
+    if (user_answers.length == obj_questions.answers.length) {
         checkUserResults();
         
         handleBntTimer(document.querySelector("#btn_exam"));
@@ -156,66 +189,37 @@ function submitAnswer(event) {
         return;
     }
 
-    showQuestion();
+    const choosenQuestion = chooseQuestion();
+    showQuestion(choosenQuestion);
 }
 
-function loadAllQuestions() {
-    obj_question = {
-        questions: [
-            {
-                id: 1,
-                description: "Which of the following DataFrame operations is always classified as a narrow transformation?",
-                options: [
-                    "A. DataFrame.sort()",
-                    "B. DataFrame.distinct()",
-                    "C. DataFrame.repartition()",
-                    "D. DataFrame.select()",
-                    "E. DataFrame.join()"
-                ]
-            },
-            {
-                id: 2,
-                description: "Which of the following is the default storage level for persist() for a non-streaming\nDataFrame/Dataset?",
-                options: [
-                    "A. MEMORY_AND_DISK",
-                    "B. MEMORY_AND_DISK_SER",
-                    "C. DISK_ONLY",
-                    "D. MEMORY_ONLY_SER",
-                    "E. MEMORY_ONLY"
-                ]
-            },
-            {
-                id: 3,
-                description: "Which of the following operations is most likely to induce a skew in the size of your data's\npartitions?",
-                options: [
-                    "A. DataFrame.collect()",
-                    "B. DataFrame.cache()",
-                    "C. DataFrame.repartition(n)",
-                    "D. DataFrame.coalesce(n)",
-                    "E. DataFrame.persist()"
-                ]
-            }
-            
-        ],
-        answers: ["D", "A", "C"],
-        selectedQuestions: []
-    }
-}
+async function loadAllQuestions() {
 
-function checkUserResults()  {
-    let int_hits = 0;
-    let int_j;
+    try {
+        let obj_req = await fetch(data_url + "questions.json");
+        let obj_json1 = await obj_req.json();
 
-    for (let i = 0; i < user_answers.length; i++) {
-        int_j = obj_question.selectedQuestions[i];
-        if (user_answers[i] == obj_question.answers[int_j])
-            int_hits++;
+        if (obj_json1) {
+            obj_questions.questions = obj_json1.questions;
+        }
+
+        obj_req = await fetch(data_url + "answers.json");
+        let obj_json2 = await obj_req.json();
+
+        if (obj_json2) {
+            obj_questions.answers = obj_json2.answers;
+        }
+
+        if (
+            (obj_questions.questions.length != 0) && 
+            (obj_questions.answers.length != 0)
+        ) {
+            document.querySelector("#btn_exam").disabled = false;
+        }
+
+    } catch(e) {
+        console.log("An error occurred!\n" + e.message);
+        showMessageToUser("Loading questions failed!");
     }
 
-    showMessageToUser("You answered all of questions!<br />Your hints: " + int_hits.toString() );
-
-}
-
-function showMessageToUser(str_message) {
-    document.querySelector("#user_message").innerHTML = str_message;
 }
